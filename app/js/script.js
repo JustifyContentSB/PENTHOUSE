6ee75d474fe6b5d4db044dbbb95c0d14ec232941
JustifyContentSB <70196752+JustifyContentSB@users.noreply.github.com>
2020-12-15 20:38:52 +0300

    Сделал часть страницы на мобилке

---
+++ app/js/script.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+	$('.penthouse').slick({
+		arrows: false,
+		variableWidth: true,
+	});
+	$('.features__list').slick({
+		arrows: false,
+		variableWidth: true,
+	});
+	$('.house__list').slick({
+		arrows: false,
+		variableWidth: true,
+	});
+});
