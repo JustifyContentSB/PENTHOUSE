e2cf25155589190745f694b76f610ddcf76bb277
JustifyContentSB <70196752+JustifyContentSB@users.noreply.github.com>
2020-12-18 11:49:58 +0300

    Реализация главной страницы с адаптивом

--- app/js/script.js
+++ app/js/script.js
@@ -1,14 +1,33 @@
 $(document).ready(function(){
-	$('.penthouse').slick({
-		arrows: false,
-		variableWidth: true,
-	});
-	$('.features__list').slick({
-		arrows: false,
-		variableWidth: true,
-	});
-	$('.house__list').slick({
-		arrows: false,
-		variableWidth: true,
-	});
+	//NAV 
+	$(document).ready(function(){
+		$('.nav__toggle').on('click', function(event){
+			event.preventDefault();
+			$(this).find('.nav__toggle-icon').toggleClass("open");
+			$('.nav__inner').toggleClass('visible');
+			$('body').toggleClass('no-scroll');
+			$('.overlay').toggleClass('visible');
+		});
+		$('.overlay').on('click', function(){
+			$('.nav__toggle-icon').toggleClass("open");
+			$('.nav__inner').toggleClass('visible');
+			$(this).toggleClass('visible');
+			$('body').toggleClass('no-scroll');
+		});
+	})
+
+	//SWIPER
+	const penthouse = document.querySelector('.penthouse');
+	let penthouseSwiper = new Swiper(penthouse, {
+      slidesPerView: 'auto',
+      spaceBetween: 2
+    });
+
+    const house = document.querySelectorAll('.house__list');
+    for(let i = 0; i < house.length; i++) {
+    	let houseSwiper = new Swiper(house[i], {
+	      slidesPerView: 'auto',
+	      spaceBetween: 2
+	    });
+    }
 });
